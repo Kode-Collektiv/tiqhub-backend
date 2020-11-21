4b7595e4f8817e631ec01b28752aae36cdc52a7c
j-sattler <mail@sattlerjoshua.de>
2020-11-22 00:00:25 +0100

    Update ticker id

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import http from 'http';
-import { Socket, Server } from "socket.io";
+import {Socket, Server} from "socket.io";
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
@@ -17,7 +17,15 @@ app.get('/', function (_req, res) {
 
 io.on("connection", function (socket: Socket) {
     const tickerId = socket.handshake.query.tickerId;
-    if(tickerId) {socket.join(tickerId);}
+    if (tickerId) {
+        socket.join(tickerId);
+        console.log('received ticker id ' + tickerId)
+    }
+
+    socket.on(tickerId, (broadcast) => {
+        io.to(tickerId).emit('broadcast', broadcast);
+    });
+
 });
 
 server.listen(process.env.PORT || 3000, function () {
