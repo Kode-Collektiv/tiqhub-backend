e8e8eb30559be2f6ed235c8d7b8cfe9885f66b46
Felix Ebert <12857672+felix-ebert@users.noreply.github.com>
2020-11-21 17:16:11 +0100

    Add minimalistic websocket server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
-import express = require('express');
+import express from 'express';
+import http from 'http';
+import { Socket, Server } from "socket.io";
 
-// Create a new express application instance
 const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const io: Server = new Server(server);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+server.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+io.on("connection", function (socket: Socket) {
+    console.log("a user connected");
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
