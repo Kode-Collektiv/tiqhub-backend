7f3cb778d51fac0023194b54961397e7e3974b71
Nico Müller <nicomueller1991@googlemail.com>
2020-11-22 15:02:17 +0100

    Create JWT during ticker creation

--- src/controllers/TickerController.ts
+++ src/controllers/TickerController.ts
@@ -1,13 +1,24 @@
 import * as mongoose from 'mongoose';
 import { TickerSchema } from '../models/Ticker';
 import { Request, Response } from 'express';
+import {sign} from "jsonwebtoken";
 
 const Ticker = mongoose.model('Ticker', TickerSchema);
 
 export class TickerController {
 
+    private static _createTickerAccessToken(tickerId: string): string {
+        return sign( {tickerId: tickerId}, "WONDERFULSECRET", { expiresIn: '12h' });
+    }
+
     public createTicker(req: Request, res: Response): void {
-        const ticker = new Ticker(req.body);
+        const tickerBody = req.body;
+
+        // create Access Token
+        tickerBody.accessToken = TickerController._createTickerAccessToken(tickerBody.name);
+
+        // create Ticker and safe
+        const ticker = new Ticker(tickerBody);
         ticker.save((err, ticker) => {
             if (err) {
                 res.status(400).send(err);
