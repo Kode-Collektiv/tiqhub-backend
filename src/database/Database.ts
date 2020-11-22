a2dfbb913ebdd315b9a4c0bcfedfa553cef22ff0
Joel Bartelheimer <joel.bartelheimer@gmx.de>
2020-11-22 19:32:40 +0100

    allow real mongoDB connection via env-variable
    Signed-off-by: Joel Bartelheimer <joel.bartelheimer@gmx.de>

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -2,36 +2,42 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 
 export default function initDB(): void {
-
     mongoose.Promise = Promise;
-    const mongoServer = new MongoMemoryServer();
+    if (process.env.MONGO_URI) {
+        console.log("connect to:", process.env.MONGO_URI);
+        connectMongo(process.env.MONGO_URI);
+    } else {
+        console.log("create inMemMongo");
+        const mongoInMemServer = new MongoMemoryServer();
+        mongoInMemServer.getUri().then((mongoUri) => connectMongo(mongoUri));
+    }
+}
 
-    mongoServer.getUri().then((mongoUri) => {
-        const mongooseOpts = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
-        };
+function connectMongo(mongoUri: string) {
+    const mongooseOpts = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    };
+    
+    mongoose.connect(mongoUri, mongooseOpts);
 
-        mongoose.connect(mongoUri, mongooseOpts);
+    mongoose.connection.once('open', () => {
+        console.log(`MongoDB successfully connected to ${mongoUri}`);
+    });
 
-        mongoose.connection.once('open', () => {
-            console.log(`MongoDB successfully connected to ${mongoUri}`);
-        });
+    mongoose.connection.on('error', function (error) {
+        console.log(`"MongoDB connection has occured ${error} error`);
+    });
 
-        mongoose.connection.on('error', function (error) {
-            console.log(`"MongoDB connection has occured ${error} error`);
-        });
+    mongoose.connection.on('disconnected', function () {
+        console.log('Mongoose connection is disconnected');
+    });
 
-        mongoose.connection.on('disconnected', function(){
-            console.log('Mongoose connection is disconnected');
-        });
-
-        process.on('SIGINT', function(){
-            mongoose.connection.close(function(){
-                console.log('Mongoose connection is disconnected due to application termination');
-                process.exit(0)
-            });
+    process.on('SIGINT', function () {
+        mongoose.connection.close(function () {
+            console.log('Mongoose connection is disconnected due to application termination');
+            process.exit(0)
         });
     });
 }
